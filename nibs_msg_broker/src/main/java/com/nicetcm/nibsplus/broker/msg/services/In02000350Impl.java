c41742d2e25d3b348d0d8ba376d78641eea1fe31
KDJ <KDJ@MSDN-SPECIAL>
2014-08-07 19:41:42 +0900

    Commit at office

--- nibs_msg_broker/src/main/java/com/nicetcm/nibsplus/broker/msg/services/In02000350Impl.java
+++ nibs_msg_broker/src/main/java/com/nicetcm/nibsplus/broker/msg/services/In02000350Impl.java
@@ -1,5 +1,17 @@
 package com.nicetcm.nibsplus.broker.msg.services;
 
+/*
+ * Copyright 2014 The NIBS+ Project
+ *
+ * MSG Broker 시설물 설치/철수 정보
+ *
+ * <pre>
+ * MngSM_SaveSetFacInfo
+ * </pre>
+ *
+ *           2014. 08. 07    K.D.J.
+ */
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +20,69 @@ import org.springframework.stereotype.Service;
 import com.nicetcm.nibsplus.broker.common.MsgParser;
 import com.nicetcm.nibsplus.broker.msg.MsgBrokerData;
 import com.nicetcm.nibsplus.broker.msg.mapper.StoredProcMapper;
+import com.nicetcm.nibsplus.broker.msg.mapper.TCmSetFacInfoMapper;
+import com.nicetcm.nibsplus.broker.msg.model.TCmSetFacInfo;
 
 @Service("in02000350")
 public class In02000350Impl extends InMsgHandlerImpl {
 
     private static final Logger logger = LoggerFactory.getLogger(In02000350Impl.class);
 
-    @Autowired private StoredProcMapper splMap;
+    @Autowired private StoredProcMapper    splMap;
+    @Autowired private TCmSetFacInfoMapper cmSetFacInfoMap;
 
     @Override
     public void inMsgBizProc(MsgBrokerData safeData, MsgParser parsed) throws Exception {
 
+        TCmSetFacInfo cmSetFacInfo = new TCmSetFacInfo();
+
+        cmSetFacInfo.setOrgCd           ( parsed.getString("CM.org_cd"        ) );
+        cmSetFacInfo.setWorkSeq         ( parsed.getString("work_seq"         ) );
+        cmSetFacInfo.setWorkClass       ( parsed.getString("work_class"       ) );
+        cmSetFacInfo.setFacSeqNo        ( parsed.getString("fac_seq_no"       ) );
+        cmSetFacInfo.setCloseType       ( parsed.getString("close_type"       ) );
+        cmSetFacInfo.setChangeNo        ( parsed.getShort ("change_no"        ) );
+        cmSetFacInfo.setOrgBranchCd     ( parsed.getString("org_brch_cd"      ) );
+        cmSetFacInfo.setOrgSiteCd       ( parsed.getString("org_site_cd"      ) );
+        cmSetFacInfo.setFacType         ( parsed.getString("fac_type"         ) );
+        cmSetFacInfo.setFacCd           ( parsed.getString("fac_cd"           ) );
+        cmSetFacInfo.setFacRentYn       ( parsed.getString("fac_rent_yn"      ) );
+        cmSetFacInfo.setItemGubun       ( parsed.getString("item_gubun"       ) );
+        cmSetFacInfo.setAssetSeqno      ( parsed.getString("asset_seqno"      ) );
+        cmSetFacInfo.setFacIp           ( parsed.getString("fac_ip"           ) );
+        cmSetFacInfo.setFacGw           ( parsed.getString("fac_gw"           ) );
+        cmSetFacInfo.setFacSm           ( parsed.getString("fac_sm"           ) );
+        cmSetFacInfo.setRentFee         ( parsed.getString("rent_fee"         ) );
+        cmSetFacInfo.setDetailFacInfo   ( parsed.getString("detail_fac_info"  ) );
+        cmSetFacInfo.setCloseOrgBranchCd( parsed.getString("close_org_brch_cd") );
+        cmSetFacInfo.setCloseOrgSiteCd  ( parsed.getString("close_org_site_cd") );
+        cmSetFacInfo.setCloseFacType    ( parsed.getString("close_fac_type"   ) );
+        cmSetFacInfo.setCloseFacCd      ( parsed.getString("close_fac_cd"     ) );
+        cmSetFacInfo.setCloseAssetSeqno ( parsed.getString("close_asset_seqno") );
+        cmSetFacInfo.setUpdateDate      ( safeData.getDSysDate()                );
+        cmSetFacInfo.setUpdateUid       ( "ETCIn"                               );
+        cmSetFacInfo.setDataType        ( parsed.getString("data_type"        ) );
+
+        /*
+         * 변경신규 모두 insert 시도 후 데이터 중복 시 insert 하도록
+         */
+        try {
+            cmSetFacInfoMap.insert( cmSetFacInfo );
+        }
+        catch( org.springframework.dao.DataIntegrityViolationException de ) {
+            cmSetFacInfo.setUpdateUid       ( "ETCUp" );
+            try {
+                cmSetFacInfoMap.updateByPrimaryKeySelective( cmSetFacInfo );
+            }
+            catch( Exception e ) {
+                logger.info( "[T_CM_SET_FAC_INFO] Update Error [{}]", e.getLocalizedMessage() );
+                throw e;
+            }
+        }
+        catch( Exception e ) {
+            logger.info( "[T_CM_SET_FAC_INFO] Update Error [{}]", e.getLocalizedMessage() );
+            throw e;
+        }
     }
 
 }
--- nibs_msg_broker/src/main/java/com/nicetcm/nibsplus/broker/msg/services/In02000370Impl.java
+++ nibs_msg_broker/src/main/java/com/nicetcm/nibsplus/broker/msg/services/In02000370Impl.java
@@ -1,5 +1,17 @@
 package com.nicetcm.nibsplus.broker.msg.services;
 
+/*
+ * Copyright 2014 The NIBS+ Project
+ *
+ * MSG Broker 시설물 물품 정보
+ *
+ * <pre>
+ * MngSM_SaveSetFacItem
+ * </pre>
+ *
+ *           2014. 08. 07    K.D.J.
+ */
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,17 +20,102 @@ import org.springframework.stereotype.Service;
 import com.nicetcm.nibsplus.broker.common.MsgParser;
 import com.nicetcm.nibsplus.broker.msg.MsgBrokerData;
 import com.nicetcm.nibsplus.broker.msg.mapper.StoredProcMapper;
+import com.nicetcm.nibsplus.broker.msg.mapper.TCmSetFacItemMapper;
+import com.nicetcm.nibsplus.broker.msg.model.TCmSetFacItem;
+import com.nicetcm.nibsplus.broker.msg.model.TCmSetFacItemSpec;
 
 @Service("in02000370")
 public class In02000370Impl extends InMsgHandlerImpl {
 
     private static final Logger logger = LoggerFactory.getLogger(In02000370Impl.class);
 
-    @Autowired private StoredProcMapper splMap;
+    @Autowired private StoredProcMapper    splMap;
+    @Autowired private TCmSetFacItemMapper cmSetFacItemMap;
 
     @Override
     public void inMsgBizProc(MsgBrokerData safeData, MsgParser parsed) throws Exception {
 
+        TCmSetFacItem cmSetFacItem = new TCmSetFacItem();
+
+        /*
+         * data_type = '1' 신규라면 insert 처리 단 dup 발생시 update
+         * data_type = '3' 삭제라면 use_yn = '0', close_date = sysdate로 처리
+         */
+        if( parsed.getString("data_type").equals("1") ) {
+            cmSetFacItem.setOrgCd     ( parsed.getString("CM.org_cd"   ) );
+            cmSetFacItem.setFacType   ( parsed.getString("aft_fac_type") );
+            cmSetFacItem.setFacCd     ( parsed.getString("aft_fac_cd"  ) );
+            cmSetFacItem.setFacNm     ( parsed.getString("aft_fac_nm"  ) );
+            cmSetFacItem.setFacComNm  ( parsed.getString("aft_com_nm"  ) );
+            cmSetFacItem.setRentFee   ( parsed.getString("aft_rent_fee") );
+            cmSetFacItem.setUseYn     ( "1"                              );
+            cmSetFacItem.setInsertDate( safeData.getDSysDate()           );
+            cmSetFacItem.setCloseDate ( null                             );
+            cmSetFacItem.setUpdateDate( safeData.getDSysDate()           );
+            cmSetFacItem.setUpdateUid ( "ETCIn"                          );
+            try {
+                cmSetFacItemMap.insert( cmSetFacItem );
+            }
+            catch( org.springframework.dao.DataIntegrityViolationException de ) {
+                cmSetFacItem.setUpdateUid ( "ETCUp" );
+                try {
+                    cmSetFacItemMap.updateByPrimaryKeySelective( cmSetFacItem );
+                }
+                catch( Exception e ) {
+                    logger.info( "[T_CM_SET_FAC_ITEM] Update Error [{}]", e.getLocalizedMessage() );
+                    throw e;
+                }
+            }
+            catch( Exception e ) {
+                logger.info( "[T_CM_SET_FAC_ITEM] Insert Error [{}]", e.getLocalizedMessage() );
+                throw e;
+            }
+        }
+        else if( parsed.getString("data_type").equals("2") ) {
+            TCmSetFacItemSpec spec = new TCmSetFacItemSpec();
+
+            cmSetFacItem.setOrgCd     ( parsed.getString("CM.org_cd"   ) );
+            cmSetFacItem.setFacType   ( parsed.getString("aft_fac_type") );
+            cmSetFacItem.setFacCd     ( parsed.getString("aft_fac_cd"  ) );
+            cmSetFacItem.setFacNm     ( parsed.getString("aft_fac_nm"  ) );
+            cmSetFacItem.setFacComNm  ( parsed.getString("aft_com_nm"  ) );
+            cmSetFacItem.setRentFee   ( parsed.getString("aft_rent_fee") );
+            cmSetFacItem.setUseYn     ( "1"                              );
+            cmSetFacItem.setCloseDate ( null                             );
+            cmSetFacItem.setUpdateDate( safeData.getDSysDate()           );
+            cmSetFacItem.setUpdateUid ( "ETCUp"                          );
+
+            spec.createCriteria().andOrgCdEqualTo  ( parsed.getString("CM.org_cd"   ) )
+                                 .andFacTypeEqualTo( parsed.getString("bef_fac_type") )
+                                 .andFacCdEqualTo  ( parsed.getString("bef_fac_cd"  ) );
+            try {
+                cmSetFacItemMap.updateBySpecSelective( cmSetFacItem, spec );
+            }
+            catch( Exception e ) {
+                logger.info( "[T_CM_SET_FAC_ITEM] Update Error [{}]", e.getLocalizedMessage() );
+                throw e;
+            }
+        }
+        else if( parsed.getString("data_type").equals("3") ) {
+            cmSetFacItem.setOrgCd     ( parsed.getString("CM.org_cd"   ) );
+            cmSetFacItem.setFacType   ( parsed.getString("aft_fac_type") );
+            cmSetFacItem.setFacCd     ( parsed.getString("aft_fac_cd"  ) );
+            cmSetFacItem.setUseYn     ( "0"                              );
+            cmSetFacItem.setCloseDate ( safeData.getDSysDate()           );
+            cmSetFacItem.setUpdateDate( safeData.getDSysDate()           );
+            cmSetFacItem.setUpdateUid ( "ETCUp"                          );
+
+            try {
+                cmSetFacItemMap.updateByPrimaryKeySelective( cmSetFacItem );
+            }
+            catch( Exception e ) {
+                logger.info( "[T_CM_SET_FAC_ITEM] Update Error [{}]", e.getLocalizedMessage() );
+                throw e;
+            }
+        }
+        else {
+            throw new Exception("invalid data_type");
+        }
     }
 
 }
